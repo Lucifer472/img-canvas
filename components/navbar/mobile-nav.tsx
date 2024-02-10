54d2432d8d42a20cc6f2ee1bc7f7ba22380c41cc
Lucifer472 <JanvI@0427>
2024-02-10 12:55:20 +0530

    mobile nav fix

---
+++ components/navbar/mobile-nav.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { CompassIcon, HomeIcon, PlusSquareIcon, User } from "lucide-react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+import { Session } from "next-auth";
+
+export const MobileNav = ({ user }: { user: Session | null }) => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex md:hidden items-center justify-evenly gap-x-4 w-full h-[80px] px-2 bg-white fixed bottom-0 left-0 z-10 rounded-t-sm border-t border-slate-100 shadow-md">
+      <Link
+        href={"/"}
+        className={cn(
+          "flex flex-col items-center justify-center min-w-[25%] w-full h-full cursor-pointer px-4"
+        )}
+      >
+        <HomeIcon
+          className={cn("w-8 h-8", pathname === "/" && "text-emerald-500")}
+        />
+        <span>Home</span>
+      </Link>
+      <Link
+        href={"/explore"}
+        className={cn(
+          "flex flex-col items-center justify-center min-w-[25%] w-full h-full cursor-pointer px-4"
+        )}
+      >
+        <CompassIcon
+          className={cn(
+            "w-8 h-8",
+            pathname === "/explore" && "text-emerald-500",
+            pathname === "/popular" && "text-emerald-500"
+          )}
+        />
+        <span>Trending</span>
+      </Link>
+      <Link
+        href={"/create"}
+        className={cn(
+          "flex flex-col items-center justify-center min-w-[25%] w-full h-full cursor-pointer px-4"
+        )}
+      >
+        <PlusSquareIcon
+          className={cn(
+            "w-8 h-8",
+            pathname === "/create" && "text-emerald-500"
+          )}
+        />
+        <span>Create</span>
+      </Link>
+      <Link
+        href={user ? "/profile/" + user.user?.id : "/login"}
+        className={cn(
+          "flex flex-col items-center justify-center min-w-[25%] w-full h-full cursor-pointer px-4"
+        )}
+      >
+        <User
+          className={cn(
+            "w-8 h-8",
+            pathname === "/login" && "text-emerald-500",
+            pathname === "/profile" && "text-emerald-500"
+          )}
+        />
+        <span>{user ? "Account" : "Login"}</span>
+      </Link>
+    </div>
+  );
+};
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,19 +1,25 @@
 import Logo from "@/components/logo";
 import { SearchBar } from "@/components/navbar/search";
 import { NavLinks } from "@/components/navbar/nav-links";
+import { MobileNav } from "@/components/navbar/mobile-nav";
+
 import { auth } from "@/auth";
 
 const Navbar = async () => {
   const user = await auth();
 
   return (
-    <header className="w-full h-20 bg-white border-b boder-slate-100 shadow-sm">
-      <nav className="basic-container w-full h-full flex items-center justify-between">
-        <Logo />
-        <SearchBar />
-        <NavLinks user={user} />
-      </nav>
-    </header>
+    <>
+      <header className="w-full h-20 bg-white border-b boder-slate-100 shadow-sm">
+        <nav className="basic-container w-full h-full flex items-center justify-between">
+          <Logo />
+          <SearchBar />
+          <NavLinks user={user} />
+        </nav>
+      </header>
+
+      <MobileNav user={user} />
+    </>
   );
 };
 
