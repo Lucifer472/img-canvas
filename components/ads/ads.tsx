ca2113e64b8df33d949aa9d92b3eb63069601471
Lucifer472 <hardiksadhu472@gmail.com>
2025-02-20 12:54:41 +0530

    mission gujarat ads added

--- components/ads/ads.tsx
+++ components/ads/ads.tsx
@@ -4,45 +4,45 @@ import { AdUnit } from "next-google-adsense";
 
 export const Ads1 = () => {
   return (
-    <div className="w-full my-4 min-w-[350px] min-h-[300px]">
-      <h2 className="text-center w-full text-sm font-medium text-gray-900 mb-2">
-        Sponsored
-      </h2>
-      <AdUnit
-        publisherId="pub-6808814529890463"
-        slotId="6036611144"
-        layout="display"
-      />
+    <div className="text-center flex w-full items-center justify-center flex-col">
+      <span className="text-[10px]">SPONSORED</span>
+      <div style={{ minWidth: "336px", minHeight: "280px" }}>
+        <AdUnit
+          publisherId="pub-6808814529890463"
+          slotId={"6036611144"}
+          layout="display"
+        />
+      </div>
     </div>
   );
 };
 
 export const Ads2 = () => {
   return (
-    <div className="w-full my-4 min-w-[350px] min-h-[300px]">
-      <h2 className="text-center w-full text-sm font-medium text-gray-900 mb-2">
-        Sponsored
-      </h2>
-      <AdUnit
-        publisherId="pub-6808814529890463"
-        slotId="8324035544"
-        layout="display"
-      />
+    <div className="text-center flex w-full items-center justify-center flex-col">
+      <span className="text-[10px]">SPONSORED</span>
+      <div style={{ minWidth: "336px", minHeight: "280px" }}>
+        <AdUnit
+          publisherId="pub-6808814529890463"
+          slotId={"8324035544"}
+          layout="display"
+        />
+      </div>
     </div>
   );
 };
 
 export const Ads3 = () => {
   return (
-    <div className="w-full my-4 min-w-[350px] min-h-[300px]">
-      <h2 className="text-center w-full text-sm font-medium text-gray-900 mb-2">
-        Sponsored
-      </h2>
-      <AdUnit
-        publisherId="pub-6808814529890463"
-        slotId="4551214014"
-        layout="display"
-      />
+    <div className="text-center flex w-full items-center justify-center flex-col">
+      <span className="text-[10px]">SPONSORED</span>
+      <div style={{ minWidth: "336px", minHeight: "280px" }}>
+        <AdUnit
+          publisherId="pub-6808814529890463"
+          slotId={"4551214014"}
+          layout="display"
+        />
+      </div>
     </div>
   );
 };
